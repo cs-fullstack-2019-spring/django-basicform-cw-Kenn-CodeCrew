d7ea229af3f7010a55bbb631e1e9284be0136a23
Kenn <kenn+git@code-crew.org>
2019-02-26 09:39:34 -0600

    update

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("welcome/", views.welcome, name="welcome"),
+    path("<int:accountID>/add5/", views.add5, name="add5"),
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Account
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test URL")
+    return render(request, 'accountApp/index.html')
+
+
+def welcome(request):
+    print(request.POST)
+
+    allDataEntries = Account.objects.all()
+
+    context = {
+        'name': request.POST['username'],
+        'allAccounts': allDataEntries,
+    }
+    return render(request, 'accountApp/welcome.html', context)
+
+
+def add5(request,accountID):
+    clickedEntry = get_object_or_404(Account, pk=accountID)
+    clickedEntry.checking += 5
+    clickedEntry.save()
+
+    allEntries = Account.objects.all()
+
+    context = {
+        'name': "New Kenn",
+        'allAccounts': allEntries,
+    }
+
+    return render(request, 'accountApp/welcome.html', context)
